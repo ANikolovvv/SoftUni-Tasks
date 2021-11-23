2602dd244ba4f30ea42c79b8fbca1998a321cb54
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2021-11-23 19:34:58 +0200

    Update netherRealms.js

--- netherRealms.js
+++ netherRealms.js
@@ -1,49 +1,49 @@
 function solve(arr) {
-    let splitPattern = /[, ]+/g;
-    let str = arr.split(splitPattern);
-    let regDamage = /[+-]?\d+\.?\d*/g;
-    let symbolsReg = /[\/*]/g;
-    let healthReg = /[^0-9\.\+\-\/\*]/g;
-    let demons = {};
-  
-    for (const demon of str) {
-      let hp = 0;
-      let demege = 0;
-      if (demon.match(healthReg) !== null) {
-        let health = demon.match(healthReg);
-        hp = health.reduce((a, m) => {
-          let h = Number(m.charCodeAt());
-          return (a += h);
-        }, 0);
-      }
-      if (demon.match(regDamage) !== null) {
-        let dem = demon.match(regDamage);
-        demege = dem.map(Number).reduce((a, m) => {
-          return (a += m);
-        }, 0);
-      }
-      if (demon.match(symbolsReg) !== null) {
-        let multy = demon.match(symbolsReg);
-        for (const w of multy) {
-          if (w === "*") {
-            demege *= 2;
-          } else if (w === "/") {
-            demege /= 2;
-          }
+  let splitPattern = /[, ]+/g;
+  let str = arr.split(splitPattern);
+  let regDamage = /[+-]?\d+\.?\d*/g;
+  let symbolsReg = /[\/*]/g;
+  let healthReg = /[^0-9\.\+\-\/\*]/g;
+  let demons = {};
+
+  for (const demon of str) {
+    let hp = 0;
+    let demege = 0;
+    if (demon.match(healthReg) !== null) {
+      let health = demon.match(healthReg);
+      hp = health.reduce((a, m) => {
+        let h = Number(m.charCodeAt());
+        return (a += h);
+      }, 0);
+    }
+    if (demon.match(regDamage) !== null) {
+      let dem = demon.match(regDamage);
+      demege = dem.map(Number).reduce((a, m) => {
+        return (a += m);
+      }, 0);
+    }
+    if (demon.match(symbolsReg) !== null) {
+      let multy = demon.match(symbolsReg);
+      for (const w of multy) {
+        if (w === "*") {
+          demege *= 2;
+        } else if (w === "/") {
+          demege /= 2;
         }
       }
-      if (!demons.hasOwnProperty(demon)) {
-        demons[demon] = {};
-        demons[demon] = { hp, demege };
-      }
     }
-    let sorted = Object.entries(demons).sort((a, b) => a[0].localeCompare(b[0]));
-    for (let [key, val] of sorted) {
-      console.log(`${key} - ${val.hp} health, ${val.demege.toFixed(2)} damage`);
+    if (!demons.hasOwnProperty(demon)) {
+      demons[demon] = {};
+      demons[demon] = { hp, demege };
     }
   }
-  solve("M3ph-0.5s-0.5t0.0**");
-  //solve("M3ph1st0**");
-  solve("Gos/ho");
-  //solve("Azazel");
-  solve("M3ph1st0**, Azazel");
+  let sorted = Object.entries(demons).sort((a, b) => a[0].localeCompare(b[0]));
+  for (let [key, val] of sorted) {
+    console.log(`${key} - ${val.hp} health, ${val.demege.toFixed(2)} damage`);
+  }
+}
+solve("M3ph-0.5s-0.5t0.0**");
+solve("M3ph1st0**");
+solve("Gos/ho");
+solve("Azazel");
+solve("M3ph1st0**, Azazel");
