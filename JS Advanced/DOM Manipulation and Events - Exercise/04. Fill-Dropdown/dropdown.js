4833a1e5fbbbfa67c299049cd2343ecb105bf1d3
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-01-23 15:36:56 +0200

    Create dropdown.js

---
+++ JS Advanced/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -0,0 +1,12 @@
+function addItem() {
+  const newText = document.getElementById("newItemText");
+  const newValue = document.getElementById("newItemValue");
+
+  const option = document.createElement("option");
+  option.textContent = newText.value;
+  option.value = newValue.value;
+
+  document.getElementById("menu").appendChild(option);
+  newValue.value = "";
+  newText.value = "";
+}
