a49e8a0c789b025a870f0227efe39039e147bb25
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-01-30 13:54:45 +0200

    Create 04-filterEmployees.js

---
+++ JS Advanced/Advanced Functions/Lap/04-filterEmployees.js
@@ -0,0 +1,35 @@
+function filterEmployee(data, criteria) {
+  let dates = JSON.parse(data);
+  let [key, value] = criteria.split("-");
+
+  dates
+    .filter((x) => x[key] === value)
+    .map((x, i) => {
+      console.log(`${i}. ${x.first_name} ${x.last_name} - ${x.email}`);
+    });
+}
+
+filterEmployee(
+  `[{
+    "id": "1",
+    "first_name": "Ardine",
+    "last_name": "Bassam",
+    "email": "abassam0@cnn.com",
+    "gender": "Female"
+  }, {
+    "id": "2",
+    "first_name": "Kizzee",
+    "last_name": "Jost",
+    "email": "kjost1@forbes.com",
+    "gender": "Female"
+  },  
+{
+    "id": "3",
+    "first_name": "Evanne",
+    "last_name": "Maldin",
+    "email": "emaldin2@hostgator.com",
+    "gender": "Male"
+  }]`,
+  "gender-Female"
+);
+
