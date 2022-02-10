c2e0f1d7776a201590d2590e2bc4b9f954896f2e
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-02-10 18:19:51 +0200

    Create test.js

---
+++ JS Advanced/Prototypes and Inheritance/Exarcise/test.js
@@ -0,0 +1,27 @@
+function solution() {
+    class Employee{
+        constructor(name,age,task){
+            this.name=name,
+            this.age=age
+        }
+    }
+}
+
+const classes = solution();
+const junior = new classes.Junior("Ivan", 25);
+junior.work();
+junior.work();
+junior.salary = 5811;
+junior.collectSalary();
+const sinior = new classes.Senior("Alex", 31);
+sinior.work();
+sinior.work();
+sinior.work();
+sinior.work();
+sinior.salary = 12050;
+sinior.collectSalary();
+const manager = new classes.Manager("Tom", 55);
+manager.salary = 15000;
+manager.collectSalary();
+manager.dividend = 2500;
+manager.collectSalary();
