ba76ff3416b726875b77e42ed65eee535633e0fd
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-02-07 20:52:50 +0200

    Update App.js

--- Redux-Toolkit/redux/src/App.js
+++ Redux-Toolkit/redux/src/App.js
@@ -1,24 +1,28 @@
-
-import styles from './App.module.css';
+import { useSelector,useDispatch } from "react-redux";
+import styles from "./App.module.css";
+import { clearArray } from "./features/array/arraySlice";
 
 function App() {
+  const { counter,names } = useSelector((state) => state.arr);
+  const dispatch=useDispatch();
+
+
   return (
     <div className={styles.App}>
-        <div className={styles.box_btn}>
-            <button className={styles.btn}>
-              ADD
-            </button>
-          </div>
-        <div className={styles.box_btn}>
-            <button className={styles.btn}>
-              UPDATE
-            </button>
-          </div>
-        <div className={styles.box_btn}>
-            <button className={styles.btn}>
-              REMOVE
-            </button>
-          </div>
+      <div className={styles.box_btn}>
+        <button className={styles.btn}>ADD</button>
+      </div>
+      <div className={styles.box_btn}>
+        <button className={styles.btn}>UPDATE</button>
+      </div>
+      <div className={styles.box_btn}>
+        <button className={styles.btn}>REMOVE</button>
+      </div>
+      <div className={styles.box_btn} onClick={()=>{dispatch(clearArray())}}>
+        <button className={styles.btn}>REMOVE ALL</button>
+      </div>
+       <div>counter: {counter}</div>
+       <div>Names: {names.map(n=><div>{n.name}</div>)}</div>
     </div>
   );
 }
