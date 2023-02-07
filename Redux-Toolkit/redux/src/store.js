07502b3d646f43034d7001fb56db4613df09b0b1
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-02-07 20:53:02 +0200

    Update store.js

--- Redux-Toolkit/redux/src/store.js
+++ Redux-Toolkit/redux/src/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import arrayReducer from "./features/array/arraySlice"
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    arr:arrayReducer
+  },
 })
