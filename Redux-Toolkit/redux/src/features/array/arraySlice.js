63dc8ba4cc69f3489f9e91e98e7b9dbda96838de
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-02-09 19:57:38 +0200

    Update arraySlice.js

--- Redux-Toolkit/redux/src/features/array/arraySlice.js
+++ Redux-Toolkit/redux/src/features/array/arraySlice.js
@@ -13,8 +13,32 @@ const arraySlice = createSlice({
     clearArray: (state) => {
       state.names = [];
     },
+    removeName: (state, action) => {
+      const name = action.payload;
+      state.names = state.names.filter((x) => x.name !== name);
+      console.log(name, "remove");
+    },
+    addName: (state, action) => {
+      const name = action.payload;
+      // let newName = state.names.slice();
+      // newName.push(name);
+      // state.names = newName;
+      state.names.push(action.payload);
+    },
+    updateName: (state, action) => {
+      const c = action.payload;
+
+      state.names = state.names.filter((x) => x.name !== c.name);
+      state.names.push({ name: c.value });
+      //let newName = state.names.slice(0);
+      // let index=newName.indexOf(name);
+
+      // newName.push(name);
+      //state.names = newName;
+    },
   },
 });
 console.log(arraySlice);
-export const {clearArray}=arraySlice.actions
+export const { clearArray, removeName, addName, updateName } =
+  arraySlice.actions;
 export default arraySlice.reducer;
