59c660778aa259013331852fa409c204506702c1
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-02-09 19:57:31 +0200

    Update App.js

--- Redux-Toolkit/redux/src/App.js
+++ Redux-Toolkit/redux/src/App.js
@@ -1,28 +1,68 @@
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import styles from "./App.module.css";
-import { clearArray } from "./features/array/arraySlice";
+import {
+  clearArray,
+  removeName,
+  addName,
+  updateName,
+} from "./features/array/arraySlice";
 
 function App() {
-  const { counter,names } = useSelector((state) => state.arr);
-  const dispatch=useDispatch();
+  const { counter, names } = useSelector((state) => state.arr);
 
+  const dispatch = useDispatch();
 
   return (
     <div className={styles.App}>
       <div className={styles.box_btn}>
-        <button className={styles.btn}>ADD</button>
+        <button
+          className={styles.btn}
+          onClick={() => {
+            dispatch(addName({ name: "Hens" }));
+          }}
+        >
+          ADD
+        </button>
       </div>
       <div className={styles.box_btn}>
-        <button className={styles.btn}>UPDATE</button>
+        <button
+          className={styles.btn}
+          onClick={() => {
+            dispatch(updateName({ name: "Hens",value:'Venst'}));
+          }}
+        >
+          Update
+        </button>
       </div>
       <div className={styles.box_btn}>
-        <button className={styles.btn}>REMOVE</button>
+        <button
+          className={styles.btn}
+          onClick={() => {
+            dispatch(removeName("Hens"));
+          }}
+        >
+          REMOVE
+        </button>
       </div>
-      <div className={styles.box_btn} onClick={()=>{dispatch(clearArray())}}>
+      <div
+        className={styles.box_btn}
+        onClick={() => {
+          dispatch(clearArray());
+        }}
+      >
         <button className={styles.btn}>REMOVE ALL</button>
       </div>
-       <div>counter: {counter}</div>
-       <div>Names: {names.map(n=><div>{n.name}</div>)}</div>
+      <div>counter: {counter}</div>
+      {names !== undefined ? (
+        <div>
+          Names:{" "}
+          {names.map((n) => (
+            <div key={n.name}>{n.name}</div>
+          ))}
+        </div>
+      ) : (
+        <p>isLoading....</p>
+      )}
     </div>
   );
 }
