847300c56be22d88ab704fbc70f04e1e33fd1fb4
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-02-11 19:37:14 +0200

    Update store.js

--- Redux-Toolkit/redux/src/store.js
+++ Redux-Toolkit/redux/src/store.js
@@ -1,7 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
-import arrayReducer from "./features/array/arraySlice"
+import { configureStore } from "@reduxjs/toolkit";
+import arrayReducer from "./features/array/arraySlice";
+import modalReducer from "./features/modal/modalSlice";
+import swapiReducer from "./features/swapi/swapiSlice";
 export const store = configureStore({
   reducer: {
-    arr:arrayReducer
+    arr: arrayReducer,
+    modal: modalReducer,
+    swapi: swapiReducer,
   },
-})
+});
