1b29c3232ba78a6720517a7e47e6adab773a9783
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-05-07 14:13:10 +0300

    Update GoalItem.js

--- AwesomeProject/components/GoalItem.js
+++ AwesomeProject/components/GoalItem.js
@@ -1,21 +1,30 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props) {
   return (
-    <View  style={styles.item}>
-      <Text style={styles.text}>{props.text}</Text>
+    <View style={styles.item}>
+      <Pressable
+        android_ripple={{ color: "#dddddd" }}
+        onPress={props.onDelete.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
+        <Text style={styles.text}>{props.text}</Text>
+      </Pressable>
     </View>
   );
 }
 const styles = StyleSheet.create({
   item: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
   text: {
     color: "white",
+    padding: 8,
+  },
+  pressedItem: {
+    opacity: 0.5,
   },
 });
 export default GoalItem;
