dc8d784ed04f27d024f57a5af3b4e98f1d06029f
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-30 16:53:53 +0300

    Update test.js

--- Financial-Times/tests/test.js
+++ Financial-Times/tests/test.js
@@ -1,4 +1,6 @@
 const { expect } = require("chai");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 const url =
   "https://markets-data-api-proxy.ft.com/research/webservices/securities/v1/quotes?symbols=IB.1:IEU";
@@ -14,5 +16,49 @@ describe("Data Fetching", () => {
     const fetchData = await fetch(url + "S");
     const info = await fetchData.json();
     expect(info).to.have.own.property("error");
+  });
+});
+
+describe("HTML Tags Existence", () => {
+  let dom;
+  let vDom;
+  before(() => {
+    dom = new JSDOM(
+      '<!DOCTYPE html><html><body><div class="container"><h1 id="title">Financial Times</h1></div></body></html>'
+    );
+
+    vDom = new JSDOM(
+      '<!DOCTYPE html><html><body><div class="container"><ul><li><span>FTSE 100</span></li></ul></div></body></html>'
+    );
+  });
+
+  it("should have the <div> tag with the class Container", () => {
+    const document = dom.window.document;
+    const divElement = document.querySelector(".container");
+    expect(divElement).to.exist;
+    expect(divElement.tagName).to.equal("DIV");
+  });
+  it("should have the <h1> tag with text financial fimes and id with name title", () => {
+    const document = dom.window.document;
+    const h1Element = document.querySelector("#title");
+    expect(h1Element).to.exist;
+    expect(h1Element.tagName).to.equal("H1");
+    expect(h1Element.textContent).to.equal("Financial Times");
+  });
+  it("should have the <ul> tag", () => {
+    const document = vDom.window.document;
+    const ulElement = document.getElementsByTagName("ul");
+    expect(ulElement).to.exist;
+  });
+  it("should have the <li> tag", () => {
+    const document = vDom.window.document;
+    const liElement = document.getElementsByTagName("li");
+    expect(liElement).to.exist;
+  });
+  it("should have the <span> tag with text FTSE 100", () => {
+    const document = vDom.window.document;
+    const spanElement = document.getElementsByTagName("span")[0];
+    expect(spanElement).to.exist;
+    expect(spanElement.textContent).to.equal("FTSE 100");
   });
 });
