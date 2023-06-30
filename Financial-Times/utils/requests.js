d603e77e09f148a4b1c7b864861cc710767cb097
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-30 13:59:58 +0300

    Create requests.js

---
+++ Financial-Times/utils/requests.js
@@ -0,0 +1,27 @@
+const url =
+  "https://markets-data-api-proxy.ft.com/research/webservices/securities/v1/quotes?symbols=";
+
+export async function fetchFtseInfo() {
+  const fetchData = await fetch(`${url}FTSE:FSI`);
+  return await fetchData.json();
+}
+
+export async function fetchSpInfo() {
+  const sp500 = await fetch(url + "INX:IOM");
+  return sp500.json();
+}
+
+export async function fetchEuroDollarInfo() {
+  const euroDollar = await fetch(url + "EURUSD");
+  return euroDollar.json();
+}
+
+export async function fetchPoundDollarInfo() {
+  const poundDollar = await fetch(url + "GBPUSD");
+  return poundDollar.json();
+}
+
+export async function fetchOilInfo() {
+  const oil = await fetch(url + "IB.1:IEU");
+  return oil.json();
+}
