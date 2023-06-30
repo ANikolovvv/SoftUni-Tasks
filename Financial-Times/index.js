6d661ecb173a11ede84e3d6221c91c47c72e215b
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-06-30 13:59:23 +0300

    Create index.js

---
+++ Financial-Times/index.js
@@ -0,0 +1,72 @@
+import { domManipulationHandler } from "./utils/helper.js";
+import {
+  fetchEuroDollarInfo,
+  fetchOilInfo,
+  fetchFtseInfo,
+  fetchSpInfo,
+  fetchPoundDollarInfo,
+} from "./utils/requests.js";
+
+async function solve() {
+  const ftse = document.getElementById("ftse");
+  const sp = document.getElementById("sp");
+  const euro = document.getElementById("euro");
+  const pound = document.getElementById("pound");
+  const brent = document.getElementById("brent");
+
+  try {
+    const fetchFtse = await fetchFtseInfo();
+    if (!fetchFtse.error) {
+      domManipulationHandler(ftse, fetchFtse);
+    } else {
+      throw new Error(fetchFtse.error.message);
+    }
+  } catch (error) {
+    ftse.classList.add("red");
+    ftse.textContent = error.message;
+  }
+
+  try {
+    const fetchSp = await fetchSpInfo();
+    if (!fetchSp.error) {
+      domManipulationHandler(sp, fetchSp);
+    } else {
+      throw new Error(fetchSp.error.message);
+    }
+  } catch (error) {
+    sp.classList.add("red");
+    sp.textContent = error.message;
+  }
+
+  try {
+    const fetchEuroDollar = await fetchEuroDollarInfo();
+    if (!fetchEuroDollar.error) {
+      domManipulationHandler(euro, fetchEuroDollar);
+    }
+  } catch (error) {
+    euro.classList.add("red");
+    euro.textContent = error.message;
+  }
+
+  try {
+    const fetchPoundDollar = await fetchPoundDollarInfo();
+    if (!fetchPoundDollar.error) {
+      domManipulationHandler(pound, fetchPoundDollar);
+    }
+  } catch (error) {
+    pound.classList.add("red");
+    pound.textContent = error.message;
+  }
+
+  try {
+    const fetchOil = await fetchOilInfo();
+    if (!fetchOil.error) {
+      domManipulationHandler(brent, fetchOil);
+    }
+  } catch (error) {
+    brent.classList.add("red");
+    brent.textContent = error.message;
+    console.log(error.message)
+  }
+}
+solve();
