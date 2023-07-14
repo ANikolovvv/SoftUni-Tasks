3a2091c56600bbd0e492143b6d98fdc70ad87ce5
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-07-14 19:05:51 +0300

    Update index.js

--- Financial-Times/index.js
+++ Financial-Times/index.js
@@ -14,58 +14,31 @@ async function solve() {
   const pound = document.getElementById("pound");
   const brent = document.getElementById("brent");
 
-  try {
-    const fetchFtse = await fetchFtseInfo();
-    if (!fetchFtse.error) {
-      domManipulationHandler(ftse, fetchFtse);
-    } else {
-      throw new Error(fetchFtse.error.message);
-    }
-  } catch (error) {
-    ftse.classList.add("red");
-    ftse.textContent = error.message;
-  }
-
-  try {
-    const fetchSp = await fetchSpInfo();
-    if (!fetchSp.error) {
-      domManipulationHandler(sp, fetchSp);
-    } else {
-      throw new Error(fetchSp.error.message);
-    }
-  } catch (error) {
-    sp.classList.add("red");
-    sp.textContent = error.message;
-  }
-
-  try {
-    const fetchEuroDollar = await fetchEuroDollarInfo();
-    if (!fetchEuroDollar.error) {
-      domManipulationHandler(euro, fetchEuroDollar);
-    }
-  } catch (error) {
-    euro.classList.add("red");
-    euro.textContent = error.message;
-  }
-
-  try {
-    const fetchPoundDollar = await fetchPoundDollarInfo();
-    if (!fetchPoundDollar.error) {
-      domManipulationHandler(pound, fetchPoundDollar);
-    }
-  } catch (error) {
-    pound.classList.add("red");
-    pound.textContent = error.message;
-  }
-
-  try {
-    const fetchOil = await fetchBrentOilInfo();
-    if (!fetchOil.error) {
-      domManipulationHandler(brent, fetchOil);
-    }
-  } catch (error) {
-    brent.classList.add("red");
-    brent.textContent = error.message;
-  }
+  Promise.all([
+    fetchBrentOilInfo(),
+    fetchFtseInfo(),
+    fetchEuroDollarInfo(),
+    fetchSpInfo(),
+    fetchPoundDollarInfo(),
+  ])
+    .then(async (results) => {
+      const brentOilData = results[0];
+      const ftseData = results[1];
+      const euroDollarData = results[2];
+      const spData = results[3];
+      const poundDollarData = results[4];
+      
+      const obj = [
+        [brent, brentOilData],
+        [ftse, ftseData],
+        [euro, euroDollarData],
+        [sp, spData],
+        [pound, poundDollarData],
+      ];
+      domManipulationHandler(obj);
+    })
+    .catch((error) => {
+      console.error("An error occurred:", error);
+    });
 }
 solve();
