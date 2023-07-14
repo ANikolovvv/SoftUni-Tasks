dd220b9e418ed1c4bbd8c8fcd895844a81965cd9
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-07-14 19:05:55 +0300

    Update helper.js

--- Financial-Times/utils/helper.js
+++ Financial-Times/utils/helper.js
@@ -1,6 +1,11 @@
-export function domManipulationHandler(element, info) {
-  let data = info.data.items[0].quote.change1DayPercent.toFixed(2);
-  let className = data.includes("-") ? "red" : "green";
-  element.classList.add(className);
-  element.textContent = data + "%";
+
+export function domManipulationHandler(obj) {
+  obj.map((el) => {
+    let data = el[1].data.items[0].quote.change1DayPercent.toFixed(2);
+    let className = data.includes("-") ? "red" : "green";
+    el[0].classList.add(className);
+    el[0].textContent = data + "%";
+   
+  });
 }
+
