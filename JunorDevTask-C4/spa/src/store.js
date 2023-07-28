bda083ec95f220389f87c9a84dd73908d53b59b9
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-07-28 20:54:16 +0300

    Remove redux toolkit

--- JunorDevTask-C4/spa/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import stockReducer from "./slice/dataSlice";
-
- const store = configureStore({
-  reducer: {
-    goods: stockReducer,
-  },
-});
-export default store
