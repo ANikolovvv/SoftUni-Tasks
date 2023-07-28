39ecd9ead784f329848265f3b65eeb1c96b0949d
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-07-28 13:27:28 +0300

    Create App.js

---
+++ JunorDevTask-C4/spa/src/App.js
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import './App.css';
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/stock/:id" element={<h1>Stoks</h1>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
